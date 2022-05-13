1914c99694d9bc636904c35d1ec28ca8d50a62c5
Alps <AlpsDDJ@163.com>
2022-05-13 23:55:07 +0800

    添加常用插件 代码格式化 eslint prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,62 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path';
+import ViteRestart from 'vite-plugin-restart'
+import * as path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// import {
+//     NutuiResolve,
+//     createStyleImportPlugin,
+// } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    //设置别名
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    port: 8080, //启动端口
-    hmr: {
-      host: '127.0.0.1',
-      port: 8080
+export default () =>
+  defineConfig({
+    plugins: [
+      vue(),
+      VueJsx(),
+      VueSetupExtend(),
+      ViteRestart({
+        restart: ['my.config.[jt]s']
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia', '@vueuse/core']
+        // 可以选择auto-import.d.ts生成的位置，使用ts建议设置为'src/auto-import.d.ts'
+        // dts: 'src/auto-import.d.ts',
+      }),
+      Components({
+        // dirs: ['src/components'], // 目标文件夹
+        // extensions: ['vue', 'tsx'], // 文件类型
+        // dts: 'src/components.d.ts', // 输出文件，里面都是一些import的组件键值对
+        // ui库解析器，也可以自定义，需要安装相关UI库
+        resolvers: [NaiveUiResolver()]
+      })
+      // createStyleImportPlugin({
+      //     resolves: [NutuiResolve()],
+      // }),
+    ],
+    resolve: {
+      //设置别名
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
     },
-    // 设置 https 代理
-    proxy: {
-      '/api': {
-        target: 'your https address',
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, '')
+    server: {
+      port: 8080, //启动端口
+      hmr: {
+        host: '127.0.0.1',
+        port: 8080
+      },
+      // 设置 https 代理
+      proxy: {
+        '/api': {
+          target: 'your https address',
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
-})
+  })
