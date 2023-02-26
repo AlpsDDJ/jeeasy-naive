8525c50faae8f9a5d683bef1e01723e8935ce0f9
wei.yang <AlpsDDJ@163.com>
2023-02-26 22:43:21 +0800

    icon 调整

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -13,15 +13,34 @@
     },
     render() {
       const { iconName, type } = this.$props
-      const IconComp = defineAsyncComponent(async () => {
-        // @ts-ignore
-        const { [iconName]: comp } =
-          type === 'ionicons5' ? await import('@vicons/ionicons5') : await import('@vicons/antd')
+
+      const _iconName = ref(iconName)
+      const _type = ref(type)
+      if (iconName.indexOf(':') > 0) {
+        _iconName.value = iconName.split(':')[1]
+        _type.value = iconName.split(':')[0]
+      }
+
+      const IconComponent = defineAsyncComponent(async () => {
+        let comp
+        switch (_type.value) {
+          case 'antd':
+            comp = (await import('@vicons/antd'))[_iconName.value]
+            break
+          default:
+            comp = (await import('@vicons/ionicons5'))[_iconName.value]
+        }
         return comp
+        // const ionicons5 = (await import('@vicons/ionicons5'))[iconName]
+        // console.log(ionicons5)
+        // @ts-ignore
+        // const { [iconName]: comp } =
+        //   type === 'ionicons5' ? await import('@vicons/ionicons5') : await import('@vicons/antd')
+        // return comp
       })
       return (
         <n-icon {...this.$attrs}>
-          <IconComp />
+          <IconComponent />
         </n-icon>
       )
     }
