9bdba2b5a0a05820378333ff64ef30cdc0c5a31f
DDJ <AlpsDDJ@163.com>
2024-07-21 03:24:54 +0800

    feat(useModel): add modelKey to BaseModel for data identification
    Static property 'modelKey' was introduced to the BaseModel class to facilitate
    the identification of data objects. This can aid in the serialization and
    deserialization processes, as well as in the implementation of debugging
    and logging functionalities.

--- src/hooks/useModel/index.ts
+++ src/hooks/useModel/index.ts
@@ -1,7 +1,10 @@
 import { Field, IBaseModel } from 'easy-descriptor'
 import { EzFieldOption } from './types'
+import { DataKey } from 'easy-descriptor/dist/types'
 
 export class BaseModel<T extends BaseModel<T> = BaseModel<any>> extends IBaseModel<T, EzFieldOption> {
+  static modelKey: DataKey = 'BaseModel'
+
   @Field.Hidden()
   @Field('ID')
   id?: string
